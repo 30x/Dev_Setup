088aa9b1ecaf2bdb27b1bc2e2b514ba09984925c
jbowen93 <jbowen@apigee.com>
2016-03-01 15:13:53 -0700

    Update ctl-setup.sh to wait for k8s cluster to be up

--- Single-Node/ctl-setup.sh
+++ Single-Node/ctl-setup.sh
@@ -2,3 +2,12 @@ kubectl config set-cluster vagrant --server=https://172.17.4.99:443 --certificat
 kubectl config set-credentials vagrant-admin --certificate-authority=${PWD}/ssl/ca.pem --client-key=${PWD}/ssl/admin-key.pem --client-certificate=${PWD}/ssl/admin.pem
 kubectl config set-context vagrant --cluster=vagrant --user=vagrant-admin
 kubectl config use-context vagrant
+
+#Wait for kubernetes
+echo "Waiting for Kubernetes cluster to become available..."
+
+until $(kubectl cluster-info &> /dev/null); do
+    sleep 1
+done
+
+echo "Kubernetes cluster is up."
